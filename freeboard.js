738f8c9a62dcf659b934fe31ec00f4f3c5ffe785
Marcel <murbanek@gmx.net>
2015-02-21 10:49:37 +0100

    corrected year

--- freeboard.js
+++ freeboard.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2014 Urbiworx
+ * Copyright 2015 Urbiworx
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
