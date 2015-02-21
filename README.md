3690d3b1e3d0ed1f6d7660b0cda07d3ef3ded267
Marcel <murbanek@gmx.net>
2015-02-21 10:46:25 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# node-red-contrib-freeboard
-Freeboard Dashboard Node for Node-RED
+#Freeboard Dashboard for Node-RED
+
+#Installation
+Just install this plugin to your Node Red installation by using npm: "npm install node-red-contrib-freeboard" in your Node Red root directory
+
+#Usage
+Drag the freeboard node into your workspace and connect it appropriatley with some JSON emitting node.
+![Node RED Flow](./docs/node_red_flow.png)
+```
+[{"id":"176e3fcd.e891c","type":"freeboard","name":"[Node-RED] Freeboard","x":715,"y":233,"z":"681a559a.97e5ac","wires":[]},{"id":"41e8ab5f.be1754","type":"inject","name":"Send Random Value","topic":"","payload":"","payloadType":"date","repeat":"","crontab":"","once":false,"x":268,"y":233,"z":"681a559a.97e5ac","wires":[["c822ddf4.37dd2"]]},{"id":"c822ddf4.37dd2","type":"function","name":"","func":"msg.payload={value:Math.floor(Math.random()*100)};\nreturn msg;","outputs":1,"x":502,"y":233,"z":"681a559a.97e5ac","wires":[["176e3fcd.e891c"]]}]
+```
+
+Then go to your freeboard dashboard (is bundled with this node - no need to set up something; should run under /freeboard) and add the freeboard node from Node-RED as Datasource
+Click "ADD" below "DATASOURCE"
+![Add Datasource](./docs/freeboard_add_ds.png)
+
+Choose the Type that has the Name of your Node-RED node (in this case "[Node-RED] Freeboard") and give it a name that you want it to be available under in Freeboard.
+![Save Datasource](./docs/freeboard_save_ds.png)
+
+Now you can add a Widget using the Datasource: Click on "ADD PANE" on the top left and in the new created pane on the "+" sign in the title
+![Create Widget](./docs/freeboard_create_widget.png)
+Choose the appropriate datasource. If Freeboard already received data from your node it will also offer you available properties - so best would be to hit the inject node within Node-RED at least once.
+
+Thats it, here is your widget:
+![Widget](./docs/freeboard_widget.png)
+
+Don't forget to save your dashboard by hitting "SAVE FREEBOARD" on the top left.
---
+++ freeboard.js
@@ -0,0 +1,120 @@
+/**
+ * Copyright 2014 Urbiworx
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+var express=require("express");
+var mustache=require("mustache");
+var fs=require("fs");
+var bodyParser = require('body-parser')
+module.exports = function(RED) {
+    "use strict";
+	var dstemplate;
+	var dslib;
+	var pendingresponses=new Array();
+	fs.readFile(__dirname+"/datasource.template", function (err, data) {
+		if (err) throw err;
+		dstemplate=data.toString();
+	});
+	fs.readFile(__dirname+"/datasource.lib", function (err, data) {
+		if (err) throw err;
+		dslib=data.toString();
+	});
+	
+	var nodes=new Array();
+    function Freeboard(n) {
+        RED.nodes.createNode(this,n);
+        this.name = n.name.trim();
+		nodes.push(this);
+        var that = this;
+        this.on("input", function(msg) {
+			that.lastValue=msg.payload;
+			postValue(that.id,that.lastValue);
+        });
+		this.on("close",function() {
+			var index = nodes.indexOf(that);
+			if (index > -1) {
+				nodes.splice(index, 1);
+			}
+		});
+    }
+
+	function postValue(id,value){
+		var resp=pendingresponses;
+		pendingresponses=new Array();
+		for (var i in resp){
+			var ret={};
+			ret[id]=value
+			resp[i].end(JSON.stringify(ret));
+		}
+	}
+	
+	function interval(){
+		var resp=pendingresponses;
+		pendingresponses=new Array();
+		for (var i in resp){
+			resp[i].end(JSON.stringify({}));
+		}
+	}
+	setInterval(interval,60000);
+
+	
+	RED.httpNode.use(bodyParser.urlencoded({     
+		extended: true
+	})); 
+	RED.httpNode.use("/freeboard",express.static(__dirname + '/freeboard'));
+	RED.httpNode.get("/freeboard_api/datasources",
+		function (req,res){
+			res.write(dslib);
+			for (var i in nodes){
+				res.write(mustache.render(dstemplate,{name:nodes[i].name,display_name:nodes[i].name,description:'',id:nodes[i].id}));
+			}
+			res.end();
+		}
+	);
+	RED.httpNode.post("/freeboard_api/dashboard",
+		function (req,res){
+			fs.writeFile("freeboard_"+req.body.name+".json", req.body.content, function (err, data) {
+				if (err) throw err;
+				res.end();
+			});
+			
+		}
+	);
+	RED.httpNode.get("/freeboard_api/datasourceupdate",
+		function (req,res){
+			if(req.param("direct",false)){
+				var ret={};
+				for (var i in nodes){
+					ret[nodes[i].id]=nodes[i].lastValue;
+				}
+				res.end(JSON.stringify(ret));
+			} else {
+				pendingresponses.push(res);
+			}
+		}
+	);
+	RED.httpNode.get("/freeboard_api/dashboard/:name",
+		function (req,res){
+			fs.readFile("freeboard_"+req.params.name+".json", function (err, data) {
+				if (err) {
+					res.end(JSON.stringify({empty:true}));
+				} else {
+					res.end(data.toString());
+				}
+			});
+			
+		}
+	);
+    RED.nodes.registerType("freeboard",Freeboard);
+}
